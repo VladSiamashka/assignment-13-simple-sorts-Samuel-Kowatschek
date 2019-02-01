225e017e1dad4beee29f7aaca9e994e76b7008e2
samuel-kowatschek <samkow@gmx.at>
2019-02-01 14:03:00 +0100

    changed unittests back to normal again, started and finished sorting_algorithms.cpp

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -11,14 +11,38 @@
  *-----------------------------------------------------------------------------
 */
 #include <cstdlib>
+#include <cstdbool>
 #include "general.h"
 #include <time.h>
 
-void bubble_sort(int numbers[], int len){
-
+void bubble_sort(int numbers[], unsigned long len)
+{
+	bool hasSwapped = false;
+	do{
+		hasSwapped = false;
+		for(unsigned long i = 0; i < len-1; i++){
+			if(numbers[i] > numbers[i+1]){
+				int tmp = numbers[i];
+				numbers[i] = numbers[i+1];
+				numbers[i+1] = tmp;
+				hasSwapped = true;
+			}
+		}
+	}while(hasSwapped);
 }
-void insertion_sort(int numbers[], int len){
-
+void insertion_sort(int numbers[], unsigned long len){
+	int j;
+	for(int i = 1; i < len; i++)
+	{
+		j = i;
+		while(j>0 && numbers[j - 1] > numbers[j]){
+			int tmp = numbers[i];
+			numbers[i] = numbers[i-1];
+			numbers[i+1] = tmp;
+			j--;
+		}
+	}
+	
 }
 void init_random(int numbers[], unsigned long len)
 {
--- sorting_algorithms.h
+++ sorting_algorithms.h
@@ -21,8 +21,8 @@
 *** to initialize an array with a number of random elements.
 */
 
-void bubble_sort(int numbers[], int len);
-void insertion_sort(int numbers[], int len);
+void bubble_sort(int numbers[], unsigned long len);
+void insertion_sort(int numbers[], unsigned long len);
 void init_random(int numbers[], unsigned long len);
 
 
